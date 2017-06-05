7ad883cd6ab3c6a6b3a0cab68707a04c57697c19
Yuttana <yuttana76>
2017-06-05 23:13:48 +0700

    Add create new campGround page And new campGround function

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 var express = require("express");
 var app = express();
+var bodyparser = require("body-parser");
 
-app.set("view engine","ejs")
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs")
+
+var campgrounds = [
+        { name: "AAA", image: "http://www.herbalbear.com/pix/camping-tent-480.jpg" },
+        { name: "BBB", image: "https://s3.amazonaws.com/images.gearjunkie.com/uploads/2015/10/Hunting-Tent-700x525.jpg" },
+        { name: "CCC", image: "https://s3.amazonaws.com/images.gearjunkie.com/uploads/2015/10/Canvas-Camp-Tent-2-700x429.jpg" },
+    ];
 
 app.get("/", function (req, res) {
     res.render("landing");
 
 });
 
-app.get("/campgrounds",function(req,res){
-var campgrounds=[
-    {name:"AAA",image:"http://www.herbalbear.com/pix/camping-tent-480.jpg"},
-    {name:"BBB",image:"https://s3.amazonaws.com/images.gearjunkie.com/uploads/2015/10/Hunting-Tent-700x525.jpg"},
-    {name:"CCC",image:"https://s3.amazonaws.com/images.gearjunkie.com/uploads/2015/10/Canvas-Camp-Tent-2-700x429.jpg"},
-    ];
-
-
-res.render("campgrounds",{campgrounds:campgrounds});
+app.get("/campgrounds", function (req, res) {
+    res.render("campgrounds", { campgrounds: campgrounds });
 });
 
-app.listen(3000,function(){
+
+app.post("/campgrounds", function (req, res) {
+    //Add data form from data And add to campground array.
+    var vName=req.body.name;
+    var vImate = req.body.image;
+    var newCampGround = {name: vName, image: vImate};
+    campgrounds.push(newCampGround);
+
+    //Redirect to campgrounds
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function (req, res) {
+    res.render("new");
+});
+
+
+app.listen(3000, function () {
     console.log("The YelpCamp Server Has Started !!!");
 })
 // app.listen(process.env.PORT, process.env.IP, function () {
