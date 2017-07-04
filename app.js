0a5b602c532ece8334000c93287b6774939cd88a
Yuttana <yuttana76>
2017-07-04 23:18:03 +0700

    Change server listener PORT

--- app.js
+++ app.js
@@ -58,10 +58,10 @@ app.use("/campgrounds/:id/comments",commentRoutes); //Add prefix before call rou
 //========================
 // Server listening
 //========================
-app.listen(3000, function () {
-    console.log("The YelpCamp Server Has Started !!!");
-})
-
-// app.listen(process.env.PORT, process.env.IP, function () {
+// app.listen(3000, function () {
 //     console.log("The YelpCamp Server Has Started !!!");
 // })
+
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log("The YelpCamp Server Has Started !!!");
+})
